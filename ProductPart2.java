73f3fe1a21cfb8acd6d9568a1ba50aa8d8fd3d66
Rafael <rafaelbenitezmoreno@gmail.com>
2020-05-28 13:06:19 +0200

    Update ProductPart2.java

--- ProductPart2.java
+++ ProductPart2.java
@@ -2,9 +2,11 @@ package javaInventory;
 
 public class ProductPart2 {
 
-//En este punto, los alumnos deben crear nuevas clases ProductPart2 y
-//ProductTesterPart2 que se sumarán a la funcionalidad de la parte 1 del proyecto.
-//(Copie y pegue el código de la parte 1 en las nuevas clases de la parte 2)
+/*
+En este punto, los alumnos deben crear nuevas clases ProductPart2 y
+ProductTesterPart2 que se sumarÃ¡n a la funcionalidad de la parte 1 del proyecto.
+(Copie y pegue el cÃ³digo de la parte 1 en las nuevas clases de la parte 2)
+*/
 
 	private String numeroElemento;
 	private String nombreProducto;
@@ -65,25 +67,29 @@ public class ProductPart2 {
 		String mensaje = "";
 
 		mensaje = mensaje + "Nombre de producto " + this.nombreProducto + "\n";
-		mensaje = mensaje + "Número de Elementos " + this.numeroElemento + "\n";
-		mensaje = mensaje + "Número de unidades en existencia " + this.numeroUnidadesExistencias + "\n";
+		mensaje = mensaje + "NÃºmero de Elementos " + this.numeroElemento + "\n";
+		mensaje = mensaje + "NÃºmero de unidades en existencia " + this.numeroUnidadesExistencias + "\n";
 		mensaje = mensaje + "Precio unidad " + "$" + this.precioUnidad + "\n";
 				
-//PUNTO 3
+/*
+PUNTO 3
 				
-		//Muestre la información en ProductTesterPart2 para estos productos tal y
-		//como era en la parte 1. Además, incluya el valor de inventario para cada
-		//producto modificando el método toString() en ProductPart2.
-				
+Muestre la informaciÃ³n en ProductTesterPart2 para estos productos tal y
+como era en la parte 1. AdemÃ¡s, incluya el valor de inventario para cada
+producto modificando el mÃ©todo toString() en ProductPart2.
+*/				
 		mensaje = mensaje + "Valor de inventario " + "$" + this.calcularValor() + "\n";
 		
 
 		return mensaje;
 
 	}
-// PUNTO 2
-	//2 Cree un método en la clase ProductPart2 que calculará el valor de cada
-	// elemento del inventario, usando la cantidad disponible y el precio.
+	
+/*
+PUNTO 2
+2 Cree un mÃ©todo en la clase ProductPart2 que calcularÃ¡ el valor de cada
+elemento del inventario, usando la cantidad disponible y el precio.
+*/
 	
 	public double calcularValor()
 
