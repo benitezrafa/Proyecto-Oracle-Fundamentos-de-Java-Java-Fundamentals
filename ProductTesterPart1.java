c55d6418ce0bab6dd4d5ad03cc830b3b99c58478
Rafael <rafaelbenitezmoreno@gmail.com>
2020-05-28 13:13:32 +0200

    Update ProductTesterPart1.java

--- ProductTesterPart1.java
+++ ProductTesterPart1.java
@@ -2,31 +2,36 @@ package javaInventory;
 
 public class ProductTesterPart1 {
 		
-//6. Cree una clase principal Java denominada ProductTesterPart1 que cree e
-//inicialice seis objetos ProductPart1.
-//a) Dos de los productos deben crearse mediante el constructor por defecto
-//b) Los otros cuatro deben crearse mediante valores para los argumentos
+/*
+6. Cree una clase principal Java denominada ProductTesterPart1 que cree e
+inicialice seis objetos ProductPart1.
+a) Dos de los productos deben crearse mediante el constructor por defecto
+b) Los otros cuatro deben crearse mediante valores para los argumentos.
+*/
 	
 	public static void main(String[] args) {
 		
-		//Crear dos objetos por defecto
-		//Cuando inicializamos el objeto,  creamos con new y llamamos al constructor y verificamos
-		//cual es el objeto (ProductPar1) y constructor.
-		//Guardando esa información en producto (Aunque su valores estén en cero o null).
+/*Crear dos objetos por defecto
+Cuando inicializamos el objeto,  creamos con new y llamamos al constructor y verificamos
+cual es el objeto (ProductPar1) y constructor.
+Guardando esa informaciÃ³n en producto (Aunque su valores estÃ©n en cero o null).
+/*
 		
 		ProductPart1 producto1 = new ProductPart1();
 		ProductPart1 producto2 = new ProductPart1();
 		
-		//Creamos 4 objetos por parámetros:
+		//Creamos 4 objetos por parÃ¡metros:
 		
 		ProductPart1 producto3 = new ProductPart1("01", "El Mentalista", 2, 50);
 		ProductPart1 producto4 = new ProductPart1("02", "House", 5, 60);
 		ProductPart1 producto5 = new ProductPart1("03", "Serie V", 100, 40);
 		ProductPart1 producto6 = new ProductPart1("04", "MadMen", 4, 70);
 		
-//7. Desde ProductTesterPart1, visualice el número de producto, el nombre
-// del producto, el número de unidades en existencias y el precio de cada
-// unidad
+/*
+7. Desde ProductTesterPart1, visualice el nÃºmero de producto, el nombre
+del producto, el nÃºmero de unidades en existencias y el precio de cada
+unidad
+*/
 		
 		System.out.println(producto3.toString());
 		System.out.println(producto3.toString());
